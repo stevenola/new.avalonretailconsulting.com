4b0e9d6470badc4874bdb7523f0132be37f91067
steve fingerman <steve@avalonretailconsulting.com>
2020-07-24 22:35:52 -0500

    second commit

--- resources/views/components/front-master.blade.php
+++ resources/views/components/front-master.blade.php
@@ -26,7 +26,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="/new.avalonretailconsulting.com/public"><h2>Avalon Retail Consulting</h2></a>
+      <a class="navbar-brand" href="/new.avalonretailconsulting.com/public"><h2>Avalon Retail Consulting, Inc.</h2></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
